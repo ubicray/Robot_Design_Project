82ff8ce681c0145b71037e1f09a5eb96ac5bb737
Elmar Abbasov <ubicray@gmail.com>
2017-04-25 17:29:33 +0300

    Support for diagonal movement

--- src/utrobotex/scripts/serializer.py
+++ src/utrobotex/scripts/serializer.py
@@ -10,18 +10,12 @@ cmd = ''
 
 def callback(data):
     ser = serial.Serial('/dev/ttyACM0')
-    leftwheelbuf = 0
-    rightwheelbuf = 0
-    backwheelbuf = 0
     rospy.loginfo(rospy.get_caller_id() + "I heard something: %s", data)
-    if (data.linear.x!=0):
-        leftwheelbuf=data.linear.x*278*-1
-        rightwheelbuf=data.linear.x*278
 
-    if (data.angular.z!=0):
-        leftwheelbuf = data.angular.z * 43*-1
-        rightwheelbuf = data.angular.z * 43*-1
-        backwheelbuf = data.angular.z * 43*-1
+    leftwheelbuf=data.linear.x*278*-1
+    rightwheelbuf=data.linear.x*278
+
+    backwheelbuf = data.angular.z * 128*-1
 
     cmd = 'l' + str(int(leftwheelbuf)).zfill(4) + 'r' + str(int(rightwheelbuf)).zfill(4) + 'b' + str(int(backwheelbuf)).zfill(4) + '\n'
     rospy.loginfo(cmd)
